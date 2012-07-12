45818aa8acd5d0c05e870032c069b7e748c523a3
Tim Sawtell <timothy.sawtell@gmail.com>
2012-07-12 10:45:27 +1000

    Added creepy fade

--- gaben/ViewController.h
+++ gaben/ViewController.h
@@ -1,14 +1,7 @@
-//
-//  ViewController.h
-//  gaben
-//
-//  Created by Local Dev User on 19/06/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
 @property (strong, nonatomic) IBOutlet UIImageView *gabenImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *gabenTopImageView;
 
 @end
--- gaben/ViewController.m
+++ gaben/ViewController.m
@@ -1,56 +1,90 @@
-//
-//  ViewController.m
-//  gaben
-//
-//  Created by Local Dev User on 19/06/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
 @property (nonatomic, assign) BOOL disapprovingEyes;
 - (void)fade;
+- (void)creepyFade;
 @end
 
 @implementation ViewController
+@synthesize gabenTopImageView;
 @synthesize gabenImageView, disapprovingEyes;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    [NSTimer scheduledTimerWithTimeInterval:300 target:self selector:@selector(fade) userInfo:nil repeats:YES];
-    self.disapprovingEyes = NO; 
+    [NSTimer scheduledTimerWithTimeInterval:120 target:self selector:@selector(fade) userInfo:nil repeats:YES];
+    self.disapprovingEyes = YES; 
+    self.gabenImageView.image = [UIImage imageNamed:@"gaben1"];
+    self.gabenTopImageView.image = [UIImage imageNamed:@"gaben1Top"];
+    self.gabenTopImageView.hidden = YES;
+    double delayInSeconds = 60.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self creepyFade];
+    });
 }
 
 - (void)viewDidUnload
 {
     [self setGabenImageView:nil];
+    [self setGabenTopImageView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return YES;
+    return (interfaceOrientation == UIDeviceOrientationLandscapeLeft || interfaceOrientation == UIDeviceOrientationLandscapeRight);
+}
+
+- (void)creepyFade
+{
+    self.gabenTopImageView.alpha = 0.5f;
+    self.gabenTopImageView.hidden = NO;
+    [UIView animateWithDuration:10 animations:^{
+        self.gabenTopImageView.alpha = 0.2f;
+        self.gabenImageView.alpha = 0;
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:10 animations:^{
+            self.gabenImageView.alpha = 1;
+            self.gabenTopImageView.alpha = 0;
+        } completion:^(BOOL finished) {
+            self.gabenTopImageView.hidden = YES;
+        }];
+    }];
 }
 
 - (void)fade
 {
-    [UIView animateWithDuration:3.0f animations:^{
-        self.gabenImageView.alpha = 0.0f;
-    } completion:^(BOOL finished) {
-        [UIView animateWithDuration:3.0f animations:^{
-            if (self.disapprovingEyes) {
-                self.gabenImageView.image = [UIImage imageNamed:@"gaben"];
-            } else {
-                self.gabenImageView.image = [UIImage imageNamed:@"gaben1"];
-            }
-            self.disapprovingEyes = !self.disapprovingEyes;
-            self.gabenImageView.alpha = 1.0f;
+    if (self.disapprovingEyes) {
+        [UIView animateWithDuration:3 animations:^{
+            self.gabenImageView.alpha = 0;
+        } completion:^(BOOL finished) {
+            self.gabenImageView.image = [UIImage imageNamed:@"gaben"];
+            [UIView animateWithDuration:3 animations:^{
+                self.gabenImageView.alpha = 1;
+            }];
         }];
-    }];
+    } else {
+
+        [UIView animateWithDuration:3 animations:^{
+            self.gabenImageView.alpha = 0;
+        } completion:^(BOOL finished) {
+            self.gabenImageView.image = [UIImage imageNamed:@"gaben1"];
+            [UIView animateWithDuration:3 animations:^{
+                self.gabenImageView.alpha = 1;
+            }];
+        }];
+        
+        double delayInSeconds = 60.0;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [self creepyFade];
+        });
+    }
+    self.disapprovingEyes = !self.disapprovingEyes;
 }
 
 @end
