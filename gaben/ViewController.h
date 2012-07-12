1bf918efe1ae4325bdce908a119e1de1c2cdbbd5
Tim Sawtell <timothy.sawtell@gmail.com>
2012-07-12 14:40:05 +1000

    Added face detection stuff - the dodgy ios5 version way

--- gaben/ViewController.h
+++ gaben/ViewController.h
@@ -1,7 +1,7 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController <AVCaptureMetadataOutputObjectsDelegate>
+@interface ViewController : UIViewController <AVCaptureVideoDataOutputSampleBufferDelegate>
 @property (strong, nonatomic) IBOutlet UIImageView *gabenImageView;
 @property (strong, nonatomic) IBOutlet UIImageView *gabenTopImageView;
 
--- gaben/ViewController.m
+++ gaben/ViewController.m
@@ -1,14 +1,23 @@
 #import "ViewController.h"
+#import <CoreImage/CoreImage.h>
+#import <ImageIO/ImageIO.h>
+#import <AssertMacros.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @interface ViewController ()
 @property (nonatomic, assign) BOOL disapprovingEyes;
+@property (nonatomic, strong) CIDetector *faceDetector;
+@property (nonatomic, assign) BOOL isUsingFrontFacingCamera;
+@property (nonatomic, strong) AVCaptureStillImageOutput *stillImageOutput;
+@property (nonatomic, strong) AVCaptureVideoDataOutput *videoDataOutput;
+@property (nonatomic, assign) dispatch_queue_t videoDataOutputQueue;
 - (void)fade;
 - (void)creepyFade;
 @end
 
 @implementation ViewController
 @synthesize gabenTopImageView;
-@synthesize gabenImageView, disapprovingEyes;
+@synthesize gabenImageView, disapprovingEyes, faceDetector, isUsingFrontFacingCamera, videoDataOutput, videoDataOutputQueue, stillImageOutput;
 
 - (void)viewDidLoad
 {
@@ -25,34 +34,9 @@
         [self creepyFade];
     });
     
-    AVCaptureSession *captureSession = [[AVCaptureSession alloc] init];
-    AVCaptureDevice *videoCaptureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-    AVCaptureMetadataOutput *metaOutput = [AVCaptureMetadataOutput new];
-    NSError *error = nil;
-    AVCaptureDeviceInput *videoInput = [AVCaptureDeviceInput deviceInputWithDevice:videoCaptureDevice error:&error];
-    if (videoInput) {
-        [captureSession addInput:videoInput];
-        [captureSession addOutput:metaOutput];
-    }
-    else {
-        // Handle the failure.
-        NSLog(@"total failure");
-    }
-    [captureSession startRunning];
+    [self setupAVCapture];
 }
 
-- (void)viewDidUnload
-{
-    [self setGabenImageView:nil];
-    [self setGabenTopImageView:nil];
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation == UIDeviceOrientationLandscapeLeft || interfaceOrientation == UIDeviceOrientationLandscapeRight);
-}
 
 - (void)creepyFade
 {
@@ -83,7 +67,7 @@
             }];
         }];
     } else {
-
+        
         [UIView animateWithDuration:3 animations:^{
             self.gabenImageView.alpha = 0;
         } completion:^(BOOL finished) {
@@ -102,9 +86,152 @@
     self.disapprovingEyes = !self.disapprovingEyes;
 }
 
-- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+- (void)viewDidUnload
 {
-
+    [self setGabenImageView:nil];
+    [self setGabenTopImageView:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
 }
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIDeviceOrientationLandscapeLeft || interfaceOrientation == UIDeviceOrientationLandscapeRight);
+}
+
+
+
+// all the video bullshit happens down here
+
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
+{	
+	// got an image
+
+	CFDictionaryRef attachments = CMCopyDictionaryOfAttachments(kCFAllocatorDefault, sampleBuffer, kCMAttachmentMode_ShouldPropagate);
+
+	if (attachments)
+		CFRelease(attachments);
+	NSDictionary *imageOptions = nil;
+	UIDeviceOrientation curDeviceOrientation = [[UIDevice currentDevice] orientation];
+	int exifOrientation;
+	
+    /* kCGImagePropertyOrientation values
+     The intended display orientation of the image. If present, this key is a CFNumber value with the same value as defined
+     by the TIFF and EXIF specifications -- see enumeration of integer constants. 
+     The value specified where the origin (0,0) of the image is located. If not present, a value of 1 is assumed.
+     
+     used when calling featuresInImage: options: The value for this key is an integer NSNumber from 1..8 as found in kCGImagePropertyOrientation.
+     If present, the detection will be done based on that orientation but the coordinates in the returned features will still be based on those of the image. */
+    
+	enum {
+		PHOTOS_EXIF_0ROW_TOP_0COL_LEFT			= 1, //   1  =  0th row is at the top, and 0th column is on the left (THE DEFAULT).
+		PHOTOS_EXIF_0ROW_TOP_0COL_RIGHT			= 2, //   2  =  0th row is at the top, and 0th column is on the right.  
+		PHOTOS_EXIF_0ROW_BOTTOM_0COL_RIGHT      = 3, //   3  =  0th row is at the bottom, and 0th column is on the right.  
+		PHOTOS_EXIF_0ROW_BOTTOM_0COL_LEFT       = 4, //   4  =  0th row is at the bottom, and 0th column is on the left.  
+		PHOTOS_EXIF_0ROW_LEFT_0COL_TOP          = 5, //   5  =  0th row is on the left, and 0th column is the top.  
+		PHOTOS_EXIF_0ROW_RIGHT_0COL_TOP         = 6, //   6  =  0th row is on the right, and 0th column is the top.  
+		PHOTOS_EXIF_0ROW_RIGHT_0COL_BOTTOM      = 7, //   7  =  0th row is on the right, and 0th column is the bottom.  
+		PHOTOS_EXIF_0ROW_LEFT_0COL_BOTTOM       = 8  //   8  =  0th row is on the left, and 0th column is the bottom.  
+	};
+	
+	switch (curDeviceOrientation) {
+		case UIDeviceOrientationPortraitUpsideDown:  // Device oriented vertically, home button on the top
+			exifOrientation = PHOTOS_EXIF_0ROW_LEFT_0COL_BOTTOM;
+			break;
+		case UIDeviceOrientationLandscapeLeft:       // Device oriented horizontally, home button on the right
+			if (isUsingFrontFacingCamera)
+				exifOrientation = PHOTOS_EXIF_0ROW_BOTTOM_0COL_RIGHT;
+			else
+				exifOrientation = PHOTOS_EXIF_0ROW_TOP_0COL_LEFT;
+			break;
+		case UIDeviceOrientationLandscapeRight:      // Device oriented horizontally, home button on the left
+			if (isUsingFrontFacingCamera)
+				exifOrientation = PHOTOS_EXIF_0ROW_TOP_0COL_LEFT;
+			else
+				exifOrientation = PHOTOS_EXIF_0ROW_BOTTOM_0COL_RIGHT;
+			break;
+		case UIDeviceOrientationPortrait:            // Device oriented vertically, home button on the bottom
+		default:
+			exifOrientation = PHOTOS_EXIF_0ROW_RIGHT_0COL_TOP;
+			break;
+	}
+    
+	imageOptions = [NSDictionary dictionaryWithObject:[NSNumber numberWithInt:exifOrientation] forKey:CIDetectorImageOrientation];
+	//NSArray *features = [faceDetector featuresInImage:ciImage options:imageOptions];
+	
+    // get the clean aperture
+    // the clean aperture is a rectangle that defines the portion of the encoded pixel dimensions
+    // that represents image data valid for display.
+	CMFormatDescriptionRef fdesc = CMSampleBufferGetFormatDescription(sampleBuffer);
+	CGRect clap = CMVideoFormatDescriptionGetCleanAperture(fdesc, false /*originIsTopLeft == false*/);
+	
+	dispatch_async(dispatch_get_main_queue(), ^(void) {
+		//[self drawFaceBoxesForFeatures:features forVideoBox:clap orientation:curDeviceOrientation];
+        NSLog(@"%@", clap);
+	});
+}
+
+- (void)setupAVCapture
+{
+	NSError *error = nil;
+	
+	AVCaptureSession *session = [AVCaptureSession new];
+	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone)
+	    [session setSessionPreset:AVCaptureSessionPreset640x480];
+	else
+	    [session setSessionPreset:AVCaptureSessionPresetPhoto];
+	
+    // Select a video device, make an input
+	AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+	AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+	if (error) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"Failed with error %d", (int)[error code]]
+                                                            message:[error localizedDescription]
+                                                           delegate:nil 
+                                                  cancelButtonTitle:@"Dismiss" 
+                                                  otherButtonTitles:nil];
+        [alertView show];
+        [self teardownAVCapture];
+    }
+	
+    isUsingFrontFacingCamera = NO;
+	if ( [session canAddInput:deviceInput] )
+		[session addInput:deviceInput];
+	
+    // Make a still image output
+	self.stillImageOutput = [AVCaptureStillImageOutput new];
+	[self.stillImageOutput addObserver:self forKeyPath:@"capturingStillImage" options:NSKeyValueObservingOptionNew context:"AVCaptureStillImageIsCapturingStillImageContext"];
+	if ( [session canAddOutput:self.stillImageOutput] )
+		[session addOutput:self.stillImageOutput];
+	
+    // Make a video data output
+	videoDataOutput = [AVCaptureVideoDataOutput new];
+	
+    // we want BGRA, both CoreGraphics and OpenGL work well with 'BGRA'
+	NSDictionary *rgbOutputSettings = [NSDictionary dictionaryWithObject:
+									   [NSNumber numberWithInt:kCMPixelFormat_32BGRA] forKey:(id)kCVPixelBufferPixelFormatTypeKey];
+	[self.videoDataOutput setVideoSettings:rgbOutputSettings];
+	[self.videoDataOutput setAlwaysDiscardsLateVideoFrames:YES]; // discard if the data output queue is blocked (as we process the still image)
+    
+    // create a serial dispatch queue used for the sample buffer delegate as well as when a still image is captured
+    // a serial dispatch queue must be used to guarantee that video frames will be delivered in order
+    // see the header doc for setSampleBufferDelegate:queue: for more information
+	self.videoDataOutputQueue = dispatch_queue_create("VideoDataOutputQueue", DISPATCH_QUEUE_SERIAL);
+	[self.videoDataOutput setSampleBufferDelegate:self queue:self.videoDataOutputQueue];
+	
+    if ( [session canAddOutput:self.videoDataOutput] )
+		[session addOutput:self.videoDataOutput];
+	[[self.videoDataOutput connectionWithMediaType:AVMediaTypeVideo] setEnabled:NO];
+	
+	[session startRunning];
+}
+
+- (void)teardownAVCapture
+{
+	if (self.videoDataOutputQueue)
+		dispatch_release(self.videoDataOutputQueue);
+}
+
 
 @end
